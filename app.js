54a275271387302c90acf4e7c7b7642c72c059c7
Claudio Diaz <csdiazleiva@gmail.com>
2024-08-08 09:07:35 -0400

    revisar js y css

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Ejecutando el sistema")
+<div class="contenido__descripcion__observaciones">
+<h3>Ningún mensaje fue encontrado</h3>
+<p>Ingresa el texto que desees encriptar o desencriptar.</p>
+</div>
